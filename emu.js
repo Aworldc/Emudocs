53bac5f5af21d2798fead5aad9c06a5f532d5709
Aworldc <83331712+Aworldc@users.noreply.github.com>
2022-06-13 10:19:05 +1000

    Update emudocs core to new version'

--- emu.js
+++ emu.js
@@ -1,71 +1,139 @@
-console.log('importing express...')
+console.log("Emudocs v1.0.0 by Aworldc")
+
 const express = require('express')
-console.log('importing ejs...')
 const ejs = require('ejs')
-console.log('importing marked...')
 const marked = require('marked')
-console.log('importing fs...')
 const fs = require('fs')
-console.log('importing os...')
 const os = require('os')
 
-var dl = fs.readFileSync('views/your_site/special/contents-label.txt')
+// try{
+    var dl = fs.readFileSync('views/your_site/special/contents-label.txt')
 
-console.log('creating app...')
-const app = express()
-app.set('view engine', 'ejs');
+    if (process.argv[2]){
+        if (process.argv[2] == "serve"){
+            const app = express()
+            app.set('view engine', 'ejs');
+        
+            var navs = []
+            var pgs = fs.readdirSync('views/your_site/general')
+        
+            cts = []
+        
+            pgs.forEach((page) => {
+                url = '/' + dl + '/' + page.replace(/\s/g , "-")
+                cts.push({
+                    name: page.slice(0, -3),
+                    url: url
+                })
+            })
+        
+            app.get('/', (req, res) => {
+                var nvvy = fs.readFileSync('./views/your_site/special/header.ejs', 'utf-8')
+                res.render('your_site/special/home', {navbar: ejs.render(nvvy, {data: navs, dl: dl})})
+            })
+        
+            app.get('/' + dl, (req, res) => {
+                var nvvy = fs.readFileSync('./views/your_site/special/header.ejs', 'utf-8')
+                res.render('your_site/special/contents', {navbar: ejs.render(nvvy, {data: navs, dl: dl}), pages: cts})
+            })
+        
+            pgs.forEach((page) => {
+                url = '/' + dl + '/' + page.replace(/\s/g , "-")
+                app.get(url, (req, res) => {
+                    var nvvy = fs.readFileSync('./views/your_site/special/header.ejs', 'utf-8')
+                    var view = 'views/your_site/general/' + page
+                    var content = marked.parse(fs.readFileSync(view, 'utf-8'))
+                    res.render('your_site/special/page', {content: content, navbar: ejs.render(nvvy, {data: navs, dl: dl}), pages: cts, pname: page.slice(0, -3)})
+                })
+            })
+        
+            app.listen(3000, () => {
+                console.clear()
+                console.log(`         _____ __  __ _   _ ____   ___   ____ ____  
+        | ____|  \\/  | | | |  _ \\ / _ \\ / ___/ ___| 
+        |  _| | |\\/| | | | | | | | | | | |   \\___ \\ 
+        | |___| |  | | |_| | |_| | |_| | |___ ___) |
+        |_____|_|  |_|\\___/|____/ \\___/ \\____|____/ `)
+                console.log('\n        internal server running on port 3000 :)')
+            })
+        } else if(process.argv[2] == "build"){
+            console.clear()
+            console.log(`         _____ __  __ _   _ ____   ___   ____ ____  
+        | ____|  \\/  | | | |  _ \\ / _ \\ / ___/ ___| 
+        |  _| | |\\/| | | | | | | | | | | |   \\___ \\ 
+        | |___| |  | | |_| | |_| | |_| | |___ ___) |
+        |_____|_|  |_|\\___/|____/ \\___/ \\____|____/ `)
+            console.log('\n        Building your site...')
 
-var navs = []
+            if (!fs.existsSync('./out')){
+                fs.mkdirSync('./out');
+            }
 
-console.log('indexing articles...')
-var pgs = fs.readdirSync('views/your_site/general')
+            if (!fs.existsSync('./out/docs')){
+                fs.mkdirSync('./out/docs');
+            }
 
-cts = []
+            var navs = []
+            var pgs = fs.readdirSync('./views/your_site/general')
+            cts = []
+        
+            pgs.forEach((page) => {
+                url = '/' + dl + '/' + page.replace(/\s/g , "-")
+                cts.push({
+                    name: page.slice(0, -3),
+                    url: url.split('.').slice(0, -1).join('.') + '.html'.toLowerCase()
+                })
+            })
+        
+            var nvvy = fs.readFileSync('./views/your_site/special/header.ejs', 'utf-8')
+            fs.writeFileSync('./out/index.html', ejs.render(fs.readFileSync('./views/your_site/special/home.ejs', 'utf-8'), {navbar: ejs.render(nvvy, {data: navs, dl: dl})}))
+            fs.writeFileSync(`./out/${dl}/index.html`, ejs.render(fs.readFileSync('./views/your_site/special/contents.ejs', 'utf-8'), {navbar: ejs.render(nvvy, {data: navs, dl: dl}), pages: cts}))
+        
+            pgs.forEach((page) => {
+                url = './out/docs/' + page.replace(/\s/g , "-").split('.').slice(0, -1).join('.').toLowerCase() + '.html'
+                var nvvy = fs.readFileSync('./views/your_site/special/header.ejs', 'utf-8')
+                var view = 'views/your_site/general/' + page
+                var content = marked.parse(fs.readFileSync(view, 'utf-8'))
+                fs.writeFileSync(url, ejs.render(fs.readFileSync('./views/your_site/special/page.ejs', 'utf-8'), {content: content, navbar: ejs.render(nvvy, {data: navs, dl: dl}), pages: cts, pname: page.slice(0, -3)}))
+            })
 
-pgs.forEach((page) => {
-    console.log(`processing ${page}...`)
-    url = '/' + dl + '/' + page.replace(/\s/g , "-")
-    cts.push({
-        name: page.slice(0, -3),
-        url: url
-    })
-})
-
-console.log(navs)
-console.log(pgs)
-
-console.log('configuring homepage...')
-app.get('/', (req, res) => {
-    var nvvy = fs.readFileSync('./views/your_site/special/header.ejs', 'utf-8')
-    res.render('your_site/special/home', {navbar: ejs.render(nvvy, {data: navs, dl: dl})})
-})
-
-console.log('configuring contents...')
-app.get('/' + dl, (req, res) => {
-    var nvvy = fs.readFileSync('./views/your_site/special/header.ejs', 'utf-8')
-    res.render('your_site/special/contents', {navbar: ejs.render(nvvy, {data: navs, dl: dl}), pages: cts})
-})
-
-console.log('configuring artcles...')
-pgs.forEach((page) => {
-    console.log(`processing ${page}...`)
-    url = '/' + dl + '/' + page.replace(/\s/g , "-")
-    app.get(url, (req, res) => {
-        var nvvy = fs.readFileSync('./views/your_site/special/header.ejs', 'utf-8')
-        var view = 'views/your_site/general/' + page
-        var content = marked(fs.readFileSync(view, 'utf-8'))
-        res.render('your_site/special/page', {content: content, navbar: ejs.render(nvvy, {data: navs, dl: dl}), pages: cts, pname: page.slice(0, -3)})
-    })
-    console.log(`page ${page} has been processed`)
-})
-
-console.log('starting internal server...')
-app.listen(3000, () => {
-    console.clear()
-    console.log(`     _____ __  __ _   _ ____   ___   ____ ____  
-    | ____|  \\/  | | | |  _ \\ / _ \\ / ___/ ___| 
-    |  _| | |\\/| | | | | | | | | | | |   \\___ \\ 
-    | |___| |  | | |_| | |_| | |_| | |___ ___) |
-    |_____|_|  |_|\\___/|____/ \\___/ \\____|____/ `)
-    console.log('\n    internal server running on port 3000')
-})
+            console.clear()
+            console.log(`         _____ __  __ _   _ ____   ___   ____ ____  
+        | ____|  \\/  | | | |  _ \\ / _ \\ / ___/ ___| 
+        |  _| | |\\/| | | | | | | | | | | |   \\___ \\ 
+        | |___| |  | | |_| | |_| | |_| | |___ ___) |
+        |_____|_|  |_|\\___/|____/ \\___/ \\____|____/ `)
+            console.log('\n        Built your site :)')
+        } else{
+            console.clear()
+            console.log(`         _____ __  __ _   _ ____   ___   ____ ____  
+        | ____|  \\/  | | | |  _ \\ / _ \\ / ___/ ___| 
+        |  _| | |\\/| | | | | | | | | | | |   \\___ \\ 
+        | |___| |  | | |_| | |_| | |_| | |___ ___) |
+        |_____|_|  |_|\\___/|____/ \\___/ \\____|____/ `)
+            console.log('\n        That\s not a valid parameter :(')
+            console.log('        run me with the parameter "serve" to run the dev server,')
+            console.log('        run me with the parameter "build" to build your site.')
+        }
+    } else{
+        console.clear()
+        console.log(`         _____ __  __ _   _ ____   ___   ____ ____  
+        | ____|  \\/  | | | |  _ \\ / _ \\ / ___/ ___| 
+        |  _| | |\\/| | | | | | | | | | | |   \\___ \\ 
+        | |___| |  | | |_| | |_| | |_| | |___ ___) |
+        |_____|_|  |_|\\___/|____/ \\___/ \\____|____/ `)
+        console.log('\n        you didn\'t tell me what to do :D')
+        console.log('        run me with the parameter "serve" to run the dev server,')
+        console.log('        run me with the parameter "build" to build your site.')
+    }
+// } catch(err){
+//     console.clear()
+//     console.log(`         _____ __  __ _   _ ____   ___   ____ ____  
+//         | ____|  \\/  | | | |  _ \\ / _ \\ / ___/ ___| 
+//         |  _| | |\\/| | | | | | | | | | | |   \\___ \\ 
+//         | |___| |  | | |_| | |_| | |_| | |___ ___) |
+//         |_____|_|  |_|\\___/|____/ \\___/ \\____|____/ `)
+//     console.log('\n        An error Occured :(')
+//     console.log('        --- Logs ---')
+//     console.log(":(      " + err)
+// }``
